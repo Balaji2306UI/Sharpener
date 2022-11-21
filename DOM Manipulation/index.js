d8e4438c7fa999584da72edb333999eaaca8f959
Balaji K <sribala23061997@gmail.com>
2022-11-21 21:54:42 +0530

    getElementsByClassName task

--- DOM Manipulation/index.js
+++ DOM Manipulation/index.js
@@ -1,7 +1,14 @@
-document.title = "Title from Js file"
+document.title = "Title from Js file";
 //Task for getElementById()
 const header = document.getElementById('main-header');
 header.style.border = "2px solid red";
 const listItems = document.getElementsByClassName('list-group-item');
 listItems[0].style.fontWeight = "bold";
 listItems[0].style.color = 'green';
+
+//Task for getElementsByClassName()
+//const listItems = document.getElementsByClassName('list-group-item');
+listItems[2].style.backgroundColor = "green";
+for(let i=0;i<listItems.length;i++) {
+    listItems[i].style.fontWeight = "bold";
+}
