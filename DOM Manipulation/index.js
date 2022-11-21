b57a1489a00fbd885f1a0d0664784bcd1de2a530
Balaji K <sribala23061997@gmail.com>
2022-11-21 21:42:09 +0530

    getElementById task

---
+++ DOM Manipulation/index.js
@@ -0,0 +1,7 @@
+document.title = "Title from Js file"
+//Task for getElementById()
+const header = document.getElementById('main-header');
+header.style.border = "2px solid red";
+const listItems = document.getElementsByClassName('list-group-item');
+listItems[0].style.fontWeight = "bold";
+listItems[0].style.color = 'green';
