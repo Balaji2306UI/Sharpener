84b9a34ae997d86a8d7342ea30dd7a3bd3bd2480
Balaji K <sribala23061997@gmail.com>
2022-11-30 00:23:28 +0530

    Registration Form - Multiple Users

--- RegistrationForm/index.js
+++ RegistrationForm/index.js
@@ -12,13 +12,39 @@ Email: ${emailElement.value}
 Phone: ${phoneElement.value}
 Date Time: ${dateElement.value}, ${timeDropdownElement.options[timeDropdownElement.selectedIndex].value}`);
 */   
-    let userDetails = {
-        name: formElement.value,
+    let newUser = {
+        name: nameElement.value,
         email: emailElement.value,
         phone: phoneElement.value,
         date: dateElement.value,
         time: timeDropdownElement.options[timeDropdownElement.selectedIndex].value
     }
-    localStorage.setItem('user', JSON.stringify(userDetails));
     
+    if(localStorage.getItem('userDetails') == null) {
+        localStorage.setItem('userDetails', '[]');
+    }
+    let oldData = JSON.parse(localStorage.getItem('userDetails'));
+    oldData.push(newUser);
+    localStorage.setItem('userDetails', JSON.stringify(oldData));
+
+    displayUsers();
+
 });
+
+function displayUsers() {
+    const displayElement = document.getElementById('display-users');
+    displayElement.innerHTML = "";
+    if(localStorage.getItem('userDetails') == null) {
+        localStorage.setItem('userDetails', '[]');
+    }
+    let userData = JSON.parse(localStorage.getItem('userDetails'));
+    userData.forEach(user => {
+        const newListItem = document.createElement('li');
+        newListItem.textContent = JSON.stringify(user);
+        displayElement.appendChild(newListItem)
+    });
+}
+
+window.onload = function() {
+    displayUsers();
+};
