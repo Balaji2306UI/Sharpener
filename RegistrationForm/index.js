52ee6cb7fe12196c992835db2ed88e7a5ca4ed34
Balaji K <sribala23061997@gmail.com>
2022-12-03 12:30:35 +0530

    Registration Form - Edit & Delete Users

--- RegistrationForm/index.js
+++ RegistrationForm/index.js
@@ -4,14 +4,20 @@ const emailElement = document.querySelector('#email');
 const phoneElement = document.querySelector('#phone');
 const dateElement = document.querySelector("#date");
 const timeDropdownElement = document.querySelector('select');
+const displayElement = document.getElementById('display-users');
+let editIndex = 0;
 
-formElement.addEventListener("submit", (e) => {
+formElement.addEventListener("submit", (e) => addUser(e));
+
+function getUserData() {
+    if(localStorage.getItem('userDetails') == null) {
+        return localStorage.setItem('userDetails', '[]');
+    }
+    return JSON.parse(localStorage.getItem('userDetails'));
+}
+
+function addUser(e) {
     e.preventDefault();
-   /* console.log(`Name: ${nameElement.value}
-Email: ${emailElement.value}
-Phone: ${phoneElement.value}
-Date Time: ${dateElement.value}, ${timeDropdownElement.options[timeDropdownElement.selectedIndex].value}`);
-*/   
     let newUser = {
         name: nameElement.value,
         email: emailElement.value,
@@ -19,30 +25,83 @@ Date Time: ${dateElement.value}, ${timeDropdownElement.options[timeDropdownEleme
         date: dateElement.value,
         time: timeDropdownElement.options[timeDropdownElement.selectedIndex].value
     }
-    
-    if(localStorage.getItem('userDetails') == null) {
-        localStorage.setItem('userDetails', '[]');
+    let oldData = getUserData();
+    if(e.target.querySelector('button').textContent == 'Get a call') {
+        oldData.push(newUser);
     }
-    let oldData = JSON.parse(localStorage.getItem('userDetails'));
-    oldData.push(newUser);
+    else if(e.target.querySelector('button').textContent == 'Make Changes') {
+        oldData[editIndex] = newUser;
+    }
     localStorage.setItem('userDetails', JSON.stringify(oldData));
-
+    resetForm();
     displayUsers();
-
-});
+}
 
 function displayUsers() {
-    const displayElement = document.getElementById('display-users');
-    displayElement.innerHTML = "";
-    if(localStorage.getItem('userDetails') == null) {
-        localStorage.setItem('userDetails', '[]');
-    }
-    let userData = JSON.parse(localStorage.getItem('userDetails'));
-    userData.forEach(user => {
+    let listContent = document.getElementById('list-content')
+    listContent.innerHTML = "";
+    let userData = getUserData();
+    userData.forEach((user, index) => {
+        document.getElementById('list-header').hidden = false;
         const newListItem = document.createElement('li');
+        newListItem.id = index;
         newListItem.textContent = JSON.stringify(user);
-        displayElement.appendChild(newListItem)
+        const editButton = document.createElement('button');
+        editButton.className = "edit";
+        editButton.textContent = "Edit";
+        const deleteButton = document.createElement('button');
+        deleteButton.className = "delete";
+        deleteButton.textContent = "Delete";
+        newListItem.append(deleteButton, editButton)
+        listContent.appendChild(newListItem)
     });
+}
+
+displayElement.addEventListener('click', (e) => {
+    if(e.target.classList.contains('edit')) {
+        editUser(e);
+    }
+    else if(e.target.classList.contains('delete')) {
+        deleteUser(e);
+        displayUsers();
+    }
+})
+
+function deleteUser(e) {
+    let index = parseInt(e.target.parentNode.id);
+    let userData = Array.from(JSON.parse(localStorage.getItem('userDetails')));
+    if(index == 0) {
+        userData.shift();
+    }
+    else if(index+1 == userData.length)
+        userData.pop();
+    else {
+        let subArr = userData.slice(index+1);
+        userData.splice(index, userData.length - index+1, ...subArr );
+    }
+    localStorage.setItem('userDetails', JSON.stringify(userData));
+    displayUsers()
+}
+
+function editUser(e) {
+    editIndex = parseInt(e.target.parentNode.id);
+    let userDataJSON = JSON.parse(e.target.parentNode.firstChild.textContent);
+    nameElement.value = userDataJSON.name;
+    emailElement.value = userDataJSON.email;
+    phoneElement.value = userDataJSON.phone;
+    dateElement.value = userDataJSON.date;
+    let option = Array.from(timeDropdownElement.options).find((option) => option.text == userDataJSON.time);
+    option.selected = true;
+    formElement.querySelector('button').textContent = 'Make Changes'
+}
+
+function resetForm() {
+    nameElement.value = "",
+    emailElement.value = "",
+    phoneElement.value = "",
+    dateElement.value = "",
+    timeDropdownElement.options[0].selected = true;
+    formElement.querySelector('button').textContent = 'Get a call'
 }
 
 window.onload = function() {
