7a23b1906edd2f7fee46ef5d67ec2089dd3ac1cb
Balaji K <sribala23061997@gmail.com>
2023-03-08 12:49:25 +0530

    React Expense Tracker - Organizing the components

--- react-expense-tracker/src/components/Expenses/ExpenseItem.js
+++ react-expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,6 @@
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
-import Card from './Card'
+import Card from '../UI/Card'
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
