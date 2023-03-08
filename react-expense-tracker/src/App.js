036bc37e2a88419ba4fccdbfd6c3e60e39922be3
Balaji K <sribala23061997@gmail.com>
2023-03-08 08:56:49 +0530

    React Expense Tracker - using props to pass data to component
    Used map() function to iterate over an expenses array and generate ExpenseItem component for each expense.
    Used props to pass data to the ExpenseItem component to make it more reusable

--- react-expense-tracker/src/App.js
+++ react-expense-tracker/src/App.js
@@ -1,10 +1,28 @@
 import ExpenseItem from './components/ExpenseItem'
 
 function App() {
+  const expenses = [{
+    title: "Medical Insurance Premium",
+    amount: "Rs.10000",
+    date: "08-03-2023",
+    location: "Online"
+  },
+  {
+    title: "Grocery",
+    amount: "Rs.4000",
+    date: "07-03-2023",
+    location: "Pheonix Marketcity"
+  }
+];
+
   return (
     <div>
       <h1>Getting started with React</h1>
-      <ExpenseItem />
+      {
+        expenses.map((expense)=>{
+          return <ExpenseItem title={expense.title} amount={expense.amount} location={expense.location} date={expense.date}/>
+        })
+      }
     </div>
   );
 }
--- react-expense-tracker/src/components/ExpenseItem.js
+++ react-expense-tracker/src/components/ExpenseItem.js
@@ -1,18 +1,14 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const expenseDate = new Date().toISOString();
-    const expenseTitle = "Medical Insurance Premium";
-    const expensePrice = "Rs.10000";
-    const expenseLocation = "Insurance"
+function ExpenseItem(props) {
 
     return (
         <div className='expense-item'>
-            <div>{expenseDate}</div>
+            <div>{props.date}</div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <h2>{expenseLocation}</h2>
-                <div className='expense-item__price'>{expensePrice}</div>
+                <h2>{props.title}</h2>
+                <h2>{props.location}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
             </div>
         </div>
     );
