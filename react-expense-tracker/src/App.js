9d37cf20bff24024a94981cc85096ce4d03bd9b1
Balaji K <sribala23061997@gmail.com>
2023-03-11 14:49:58 +0530

    React Expense Tracker - Lifting the state up
    An important pattern to pass data from child to parent component.

--- react-expense-tracker/src/App.js
+++ react-expense-tracker/src/App.js
@@ -1,27 +1,35 @@
-import Expenses from './components/Expenses/Expenses';
-import ExpenseForm from './components/Expenses/ExpenseForm/ExpenseForm';
+import { useState } from 'react';
+import Expenses from "./components/Expenses/Expenses";
+import ExpenseForm from "./components/Expenses/ExpenseForm/ExpenseForm";
 
 function App() {
-  const expenses = [{
-    title: "Medical Insurance Premium",
-    amount: "Rs.10000",
-    date: new Date(2023,2,8), //new Date(year, month, day) => month starts with 0; 0 means January
-    location: "Online"
-  },
-  {
-    title: "Grocery",
-    amount: "Rs.4000",
-    date: new Date(2023,2,7),
-    location: "Pheonix Marketcity"
-  }
-];
+    const expenses = [
+        {
+            title: "Medical Insurance Premium",
+            amount: "Rs.10000",
+            date: new Date(2023, 2, 8), //new Date(year, month, day) => month starts with 0; 0 means January
+            location: "Online",
+        },
+        {
+            title: "Grocery",
+            amount: "Rs.4000",
+            date: new Date(2023, 2, 7),
+            location: "Pheonix Marketcity",
+        },
+    ];
 
-  return (
-    <div>
-      <ExpenseForm />
-      <Expenses data={expenses} />
-    </div>
-  );
+	const [expensesData, setExpensesData] = useState(expenses);
+
+	function onAddNewExpense(expense) {
+		setExpensesData([...expenses, expense]);
+	}
+
+    return (
+        <div>
+            <ExpenseForm onAddNewExpense={onAddNewExpense}/>
+            <Expenses data={expensesData} />
+        </div>
+    );
 }
 
 export default App;
