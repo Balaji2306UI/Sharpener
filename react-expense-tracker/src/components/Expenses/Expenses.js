99f3b24a272dfdc1aa3cd999d0a747b5d0702560
Balaji K <sribala23061997@gmail.com>
2023-03-12 20:06:52 +0530

    React Expense Tracker - Adding Year Filter

--- react-expense-tracker/src/components/Expenses/Expenses.js
+++ react-expense-tracker/src/components/Expenses/Expenses.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
-import './Expenses.css'
+import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
 
 
 function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    };
     return (
         <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
         {
             props.data.map((expense)=>{
                 return <ExpenseItem key={props.data.indexOf(expense)} title={expense.title} amount={expense.amount} location={expense.location} date={expense.date}/>
