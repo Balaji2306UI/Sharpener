e6fb05049dbe5c65cc86ccecd81e0eb4dbe9d60d
Balaji K <sribala23061997@gmail.com>
2023-03-14 20:14:51 +0530

    React Udemy Project 2 - Disabling button when input field is empty

--- react-udemy-project-2/src/components/CourseGoals/CourseInput/CourseInput.js
+++ react-udemy-project-2/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,23 +5,31 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
+    if(event.target.value.trim().length === 0){
+      setIsValid(false);
+    }
+    else {
+      setIsValid(true);
+    }
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
     props.onAddGoal(enteredValue);
+    setEnteredValue('');
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${isValid ? '' : 'invalid'}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button type="submit" disabled={!isValid}>Add Goal</Button>
     </form>
   );
 };
